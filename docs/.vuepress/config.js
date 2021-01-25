4b50ad82b079ca651a4189dff9ef98ada224cf0c
ScottMark <xiaoma_hebut@163.com>
2021-01-25 09:57:32 +0800

    modify file & add favicon

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,6 +1,9 @@
 module.exports = {
   title: '2021年学生组会',
   description: 'LAMOST 运行和发展中心',
+  head: [
+    ['link', {rel:'shortcut icon', href:'/favicon.ico'}] 
+  ],
   plugins: [
     '@vuepress/back-to-top',
     '@vuepress/medium-zoom',
@@ -17,13 +20,10 @@ module.exports = {
     nav: [
       { text: '使用说明', link: '/Demo/' },
       { text: '秦丽', link: '/qinli/'},
-      { text: '李硕', link: '/lishuo/'},
       { text: '鲁燕', link: '/luyan/'},
       { text: '陈静', link: '/chenjing/' },
-      { text: '陈向蕾', link: '/chenxianglei/'},
       { text: '韩博冲', link: '/hanbochong/'},
       { text: '屈彩霞', link: '/qucaixia/'},
-      { text: '张允晋', link: '/zhangyunjin/'},
       { text: '何徐江', link: '/hexujiang/' },
       { text: '马晓骁', link: '/maxiaoxiao/'}
     ],
@@ -33,432 +33,95 @@ module.exports = {
     docsRepo: 'lamost-org/GroupMeeting/',
     docsDir: '/',
     sidebar:{
-
-      /* 使用说明 */
-      '/Demo/':[
-        '',
-        'rules',
-        'commit',
-        'advanced',
-      ],
-      /* ============================================================================= */
-
-      /* 马晓骁 */
-      '/maxiaoxiao/':[
-        {
-          title: '01月',  //组名
-          children: ['1', '2', '3', '4', ],   //该分组下要显示的文件的目录
-        },
-        {
-          title: '02月',
-        },
-        {
-          title: '03月',
-        },
-        {
-          title: '04月',
-        },
-        {
-          title: '05月',
-        },
-        {
-          title: '06月',
-        },
-        {
-          title: '07月',
-                  },
-        {
-          title: '08月',
-                  },
-        {
-          title: '09月',
-                  },
-        {
-          title: '10月',
-                 },
-        {
-          title: '11月',
-                  },
-        {
-          title: '12月',
-        },
-      ],
-      /* ============================================================================= */
-
-      /* 何徐江 */
-      '/hexujiang/':[
-        {
-          title: '01月',  //组名
-          children: ['1', '2', '3', '4', ],   //该分组下要显示的文件的目录
-        },
-        {
-          title: '02月',
-        },
-        {
-          title: '03月',
-        },
-        {
-          title: '04月',
-        },
-        {
-          title: '05月',
-        },
-        {
-          title: '06月',
-        },
-        {
-          title: '07月',
-                  },
-        {
-          title: '08月',
-                  },
-        {
-          title: '09月',
-                  },
-        {
-          title: '10月',
-                 },
-        {
-          title: '11月',
-                  },
-        {
-          title: '12月',
-        },
-      ],
-      /* ============================================================================= */
-
-      /* 李硕 */
-      '/lishuo/':[
-        {
-          title: '01月',  //组名
-        },
-        {
-          title: '02月',
-        },
-        {
-          title: '03月',
-        },
-        {
-          title: '04月',
-        },
-        {
-          title: '05月',
-        },
-        {
-          title: '06月',
-        },
-        {
-          title: '07月',
-                  },
-        {
-          title: '08月',
-                  },
-        {
-          title: '09月',
-                  },
-        {
-          title: '10月',
-                 },
-        {
-          title: '11月',
-                  },
-        {
-          title: '12月',
-        },
-      ],
-      /* ============================================================================= */
-      
-      /* 鲁燕 */
-      '/luyan/':[
-        {
-          title: '01月',  //组名
-          children: ['2', ],   //该分组下要显示的文件的目录
-        },
-        {
-          title: '02月',
-        },
-        {
-          title: '03月',
-        },
-        {
-          title: '04月',
-        },
-        {
-          title: '05月',
-        },
-        {
-          title: '06月',
-        },
-        {
-          title: '07月',
-                  },
-        {
-          title: '08月',
-                  },
-        {
-          title: '09月',
-                  },
-        {
-          title: '10月',
-                 },
-        {
-          title: '11月',
-                  },
-        {
-          title: '12月',
-        },
-      ],
-      /* ============================================================================= */
-
-      /* 陈静 */
-      '/chenjing/':[
-        {
-          title: '01月',  //组名
-          children: ['2', '3', '4']
-        },
-        {
-          title: '02月',
-        },
-        {
-          title: '03月',
-        },
-        {
-          title: '04月',
-        },
-        {
-          title: '05月',
-        },
-        {
-          title: '06月',
-        },
-        {
-          title: '07月',
-                  },
-        {
-          title: '08月',
-                  },
-        {
-          title: '09月',
-                  },
-        {
-          title: '10月',
-                 },
-        {
-          title: '11月',
-                  },
-        {
-          title: '12月',
-        },
-      ],
-      /* ============================================================================= */
-
-      /* 陈向蕾 */
-      '/chenxianglei/':[
-        {
-          title: '01月',  //组名
-        },
-        {
-          title: '02月',
-        },
-        {
-          title: '03月',
-        },
-        {
-          title: '04月',
-        },
-        {
-          title: '05月',
-        },
-        {
-          title: '06月',
-        },
-        {
-          title: '07月',
-                  },
-        {
-          title: '08月',
-                  },
-        {
-          title: '09月',
-                  },
-        {
-          title: '10月',
-                 },
-        {
-          title: '11月',
-                  },
-        {
-          title: '12月',
-        },
-      ],
-      /* ============================================================================= */
-
-      /* 韩博冲 */
-      '/hanbochong/':[
-        {
-          title: '01月',  //组名
-          children: ['2', '4']
-        },
-        {
-          title: '02月',
-        },
-        {
-          title: '03月',
-        },
-        {
-          title: '04月',
-        },
-        {
-          title: '05月',
-        },
-        {
-          title: '06月',
-        },
-        {
-          title: '07月',
-                  },
-        {
-          title: '08月',
-                  },
-        {
-          title: '09月',
-                  },
-        {
-          title: '10月',
-                 },
-        {
-          title: '11月',
-                  },
-        {
-          title: '12月',
-        },
-      ],
-      /* ============================================================================= */
-
-      /* 屈彩霞 */
-      '/qucaixia/':[
-        {
-          title: '01月',  //组名
-          children: ['2', '3', '4', ]
-        },
-        {
-          title: '02月',
-        },
-        {
-          title: '03月',
-        },
-        {
-          title: '04月',
-        },
-        {
-          title: '05月',
-        },
-        {
-          title: '06月',
-        },
-        {
-          title: '07月',
-                  },
-        {
-          title: '08月',
-                  },
-        {
-          title: '09月',
-                  },
-        {
-          title: '10月',
-                 },
-        {
-          title: '11月',
-                  },
-        {
-          title: '12月',
-        },
-      ],
-      /* ============================================================================= */
-      
-      /* 张允晋 */
-      '/zhangyunjin/':[
-        {
-          title: '01月',  //组名
-        },
-        {
-          title: '02月',
-        },
-        {
-          title: '03月',
-        },
-        {
-          title: '04月',
-        },
-        {
-          title: '05月',
-        },
-        {
-          title: '06月',
-        },
-        {
-          title: '07月',
-                  },
-        {
-          title: '08月',
-                  },
-        {
-          title: '09月',
-                  },
-        {
-          title: '10月',
-                 },
-        {
-          title: '11月',
-                  },
-        {
-          title: '12月',
-        },
-      ],
-      /* ============================================================================= */
-
-      /* 秦丽*/
-      '/qinli/':[
-      {
-        title: '01月',
-        children: ['2', '3', '4', ],  //组名
-      },
-      {
-        title: '02月',
-      },
-      {
-        title: '03月',
-      },
-      {
-        title: '04月',
-      },
-      {
-        title: '05月',
-      },
-      {
-        title: '06月',
-      },
-      {
-        title: '07月',
-                },
-      {
-        title: '08月',
-                },
-      {
-        title: '09月',
-                },
-      {
-        title: '10月',
-                },
-      {
-        title: '11月',
-                },
-      {
-        title: '12月',
-      },
-    ],
-    /* ============================================================================= */
+      '/Demo/': DemoSidebarConf(),        // 使用说明
+      '/maxiaoxiao/': mxxSidebarConf(),   // 马晓骁
+      '/hexujiang/': hxjSidebarConf(),    // 何徐江
+      '/luyan/': lySidebarConf(),         // 鲁燕
+      '/chenjing/': cjSidebarConf(),      // 陈静
+      '/hanbochong/': hbcSidebarConf(),   // 韩博冲
+      '/qucaixia/': qcxSidebarConf(),     // 屈彩霞
+      '/qinli/': qlSidebarConf(),         // 秦丽
     }
   }
 } 
+
+
+// Demo
+function DemoSidebarConf() {
+  return [
+    '',
+    'rules',
+    'commit',
+    'advanced',
+  ];
+}
+
+// 马晓骁
+function mxxSidebarConf() {
+  return [
+    {
+      title: '01月',  //组名
+      children: ['1', '2', '3', '4', ],   //该分组下要显示的文件的目录
+    },
+  ];
+}
+
+// 何徐江
+function hxjSidebarConf() {
+  return [
+    {
+      title: '01月',  //组名
+      children: ['1', '2', '3', '4', ],   //该分组下要显示的文件的目录
+    },
+  ];
+}
+
+// 鲁燕
+function lySidebarConf() {
+  return [
+    {
+      title: '01月',  //组名
+      children: ['2', ],   //该分组下要显示的文件的目录
+    },
+  ];
+}    
+
+// 陈静
+function cjSidebarConf() {
+  return [
+    {
+      title: '01月',  //组名
+      children: ['2', '3', '4']
+    },
+  ];
+}
+
+// 韩博冲
+function hbcSidebarConf() {
+  return [
+    {
+      title: '01月',  //组名
+      children: ['2', '4']
+    },
+  ];
+}
+
+// 屈彩霞
+function qcxSidebarConf() {
+  return [
+    {
+      title: '01月',  //组名
+      children: ['2', '3', '4', ]
+    },
+  ];
+}
+
+// 秦丽
+function qlSidebarConf() {
+  return [
+    {
+      title: '01月',
+      children: ['2', '3', '4', ],  //组名
+    },
+  ];
+}
