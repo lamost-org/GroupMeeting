6655426c775261bd1732c65877b3c28870e51098
ScottMark <xiaoma_hebut@163.com>
2021-02-13 18:58:59 +0800

    deploy gitee pages

--- deploy.sh
+++ deploy.sh
@@ -21,5 +21,6 @@ git commit -m 'deploy'
 
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
 git push -f git@github.com:lamost-org/GroupMeeting.git master:gh-pages
+git push -f git@gitee.com:lamost/GroupMeeting.git master:gh-pages
 
 cd -
